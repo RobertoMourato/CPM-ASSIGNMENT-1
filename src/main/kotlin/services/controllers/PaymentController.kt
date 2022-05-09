0d7c3c075bc2f2039851addd7377c27d973eb0f3
Roberto Mourato <pmourato@e-goi.com>
2022-05-09 21:10:33 +0100

    improv: Finished handling signatures

--- src/main/kotlin/services/controllers/PaymentController.kt
+++ src/main/kotlin/services/controllers/PaymentController.kt
@@ -1,12 +1,12 @@
 package services.controllers
 
+import com.google.gson.Gson
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import services.dtos.PaymentToken
 import services.entities.Payment
 import services.repositories.CustomerRepository
 import services.repositories.PaymentRepository
-import services.utilities.GenerateSignature
 import services.utilities.VerifySignature
 import java.text.SimpleDateFormat
 import java.util.*
@@ -20,12 +20,9 @@ class PaymentController {
     lateinit var customerRepository: CustomerRepository
 
     @PostMapping
-    fun create(@RequestBody payment: Payment, @RequestHeader("Client-Id") uuid: UUID, @RequestHeader("Request-Time") requestTime: String, @RequestHeader("Signature") signature: String): PaymentToken {
-        val dummySignature = GenerateSignature("/payments", uuid.toString(), requestTime, "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAkPfH5PP+BrXuFvcroQ5er/VASWlAfhAs8OvQfIiYZyUenHVpuThO2jNxyPL95YNpZC4hMCw/Hdqv8GFYTqO3dQIDAQABAkB+27XbTwCu0qnFAwr052RIym81TpuXNYR+V32cXNQw/WMoL4OmAF8JtxoqoX8xMUmdHbp68We0RkMMN9bms5HBAiEAyZBMlvxS8c4OXTVVLJCX8lokGFWEkq7xPBxkJgpZuRkCIQC4HpC0t2V8/8L3Vv2KZggsNLmknJKcQf9EBLiXZYiQvQIhAMQawol7gO+FfPAufWnYXB0MmmWafluAHXBQZxejpluxAiBN40A78slQE7IaiAZBoeAbM0LFTCcJSzH9SDH+eclH0QIhAKYkQq4f/MEGST8OWM5zD7EEaOYFOH1qe/dijcVB87cB", payment.toString())
-        val signatureVerifier = VerifySignature("/payments", uuid.toString(), requestTime, "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJD3x+Tz/ga17hb3K6EOXq/1QElpQH4QLPDr0HyImGclHpx1abk4Ttozccjy/eWDaWQuITAsPx3ar/BhWE6jt3UCAwEAAQ==", payment.toString(), dummySignature.signature)
-
-        println(payment)
-        println(payment.toString())
+    fun create(@RequestBody items: String, @RequestHeader("Client-Id") uuid: UUID, @RequestHeader("Request-Time") requestTime: String, @RequestHeader("Signature") signature: String): PaymentToken {
+        //val dummySignature = GenerateSignature("/payments", uuid.toString(), requestTime, "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAkPfH5PP+BrXuFvcroQ5er/VASWlAfhAs8OvQfIiYZyUenHVpuThO2jNxyPL95YNpZC4hMCw/Hdqv8GFYTqO3dQIDAQABAkB+27XbTwCu0qnFAwr052RIym81TpuXNYR+V32cXNQw/WMoL4OmAF8JtxoqoX8xMUmdHbp68We0RkMMN9bms5HBAiEAyZBMlvxS8c4OXTVVLJCX8lokGFWEkq7xPBxkJgpZuRkCIQC4HpC0t2V8/8L3Vv2KZggsNLmknJKcQf9EBLiXZYiQvQIhAMQawol7gO+FfPAufWnYXB0MmmWafluAHXBQZxejpluxAiBN40A78slQE7IaiAZBoeAbM0LFTCcJSzH9SDH+eclH0QIhAKYkQq4f/MEGST8OWM5zD7EEaOYFOH1qe/dijcVB87cB", items)
+        val signatureVerifier = VerifySignature("/payments", uuid.toString(), requestTime, "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJD3x+Tz/ga17hb3K6EOXq/1QElpQH4QLPDr0HyImGclHpx1abk4Ttozccjy/eWDaWQuITAsPx3ar/BhWE6jt3UCAwEAAQ==", items, signature)
 
         if(!signatureVerifier.isValidSignature){
             return PaymentToken("Unauthorized")
@@ -34,6 +31,8 @@ class PaymentController {
         if(Math.random() <= 0.05) {
             return PaymentToken("Invalid Operation")
         }
+
+        val payment = Gson().fromJson(items, Payment::class.java)
 
         payment.uuid = uuid
 
--- src/main/kotlin/services/entities/Payment.kt
+++ src/main/kotlin/services/entities/Payment.kt
@@ -1,7 +1,5 @@
 package services.entities
 
-import java.sql.Date
-import java.sql.Time
 import java.util.UUID
 import javax.persistence.*
 import javax.validation.constraints.NotNull
@@ -9,15 +7,14 @@ import javax.validation.constraints.NotNull
 @Entity
 @Table(name = "payments")
 class Payment(
-    @field:Id
-    @field:NotNull
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
-
     @OneToMany(cascade = [CascadeType.ALL])
     @JoinColumn(name = "pi_fk", referencedColumnName = "id")
     val items: List<Item>
 ){
+    @field:Id
+    @field:NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
     lateinit var uuid: UUID
     lateinit var token: UUID
     var price: Double = 0.0
